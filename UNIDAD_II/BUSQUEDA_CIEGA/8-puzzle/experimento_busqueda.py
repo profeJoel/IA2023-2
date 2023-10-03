6eee7458634bfbcd4e0cf16d6dfd662d8bb90d8f
profeJoel <joel.torres@ulagos.cl>
2023-10-02 22:47:01 -0300

    Algoritmos de Búsqueda Ciega

---
+++ UNIDAD_II/BUSQUEDA_CIEGA/8-puzzle/experimento_busqueda.py
@@ -0,0 +1,16 @@
+from ocho_puzzle import ocho_puzzle
+
+if __name__ == "__main__":
+    #puzzle = ocho_puzzle("123H56478")
+    puzzle = ocho_puzzle("87653H241")
+    puzzle.algoritmo_anchura()
+    #puzzle.algoritmo_profundidad()
+    #puzzle.algoritmo_anchura_evalua_hijos()
+    #puzzle.algoritmo_profundidad_evalua_hijos()
+    #puzzle.algoritmo_better_first()
+    #puzzle.algoritmo_hill_climbing()
+    #puzzle.algoritmo_beam(2,None)
+    #puzzle.algoritmo_beam(1,None)
+    #puzzle.algoritmo_beam(None,0.5)
+    #puzzle.algoritmo_beam(None,0.35)
+    #puzzle.algoritmo_beam(None,0.25)
